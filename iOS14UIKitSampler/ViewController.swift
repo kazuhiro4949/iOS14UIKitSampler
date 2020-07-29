0b707263fcd45da814fdef21d6f654dd65afbfa7
Kazuhiro Hayashi <kazuhiro.hayashi.49@gmail.com>
2020-07-29 21:48:24 +0900

    Initial Commit

---
+++ iOS14UIKitSampler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS14UIKitSampler
+//
+//  Created by kahayash on 2020/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
